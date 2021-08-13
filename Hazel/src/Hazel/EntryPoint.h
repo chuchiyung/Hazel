451c4a86200464c1458f7d26aa88483dab5ad3b1
chuchiyung <chuchiyung@gmail.com>
2021-08-13 21:20:46 +0800

    Basic Commit

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,7 +6,7 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazel hello");
+	printf("Hazel commit");
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
