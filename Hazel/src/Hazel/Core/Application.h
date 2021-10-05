ced81a4a05bdbfa35e1bb11f01d591cf06d28624
chuchiyung <chuchiyung@gmail.com>
2021-10-05 22:09:34 +0800

    First Triangle with Shader (Episode 29)

--- Hazel/src/Hazel/Core/Application.h
+++ Hazel/src/Hazel/Core/Application.h
@@ -12,6 +12,8 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Renderer/Shader.h"
+
 namespace Hazel {
 
 	class HAZEL_API Application
@@ -33,6 +35,9 @@ namespace Hazel {
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
