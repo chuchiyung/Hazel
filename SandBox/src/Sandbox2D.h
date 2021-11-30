0a4a6ac9a01b1975cdaaab25e08ccf6d7231a629
chuchiyung <chuchiyung@gmail.com>
2021-11-30 18:18:01 +0800

    Intro to Profiling (Episode 55)

--- SandBox/src/Sandbox2D.h
+++ SandBox/src/Sandbox2D.h
@@ -23,6 +23,14 @@ private:
 
 	Hazel::Ref<Hazel::Texture2D> m_CheckerboardTexture;
 
+	struct ProfileResult
+	{
+		const char* Name;
+		float Time;
+	};
+
+	std::vector<ProfileResult> m_ProfileResults;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 	glm::vec4 m_BgColor = { 0.2f, 0.2f, 0.2f, 1.0f };
 };
